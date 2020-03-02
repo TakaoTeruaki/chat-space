64666986a55337c1611645ffcc83bfa12aef60aa
TakaoTeruaki <k.teru.827@gmail.com>
2020-03-02 17:35:58 +0900

    自動更新機能を追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,50 @@
 $(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html = `<div class="message" data-message-id=${message.id}>
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=${message.id}>
+      <div class="message__upper-info">
+        <div class="message__upper-info__talkr">
+          ${message.user_name}
+        </div>
+        <div class="message__upper-info__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message__text">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+      <img src=${message.image} >
+    </div>`
+}else if (message.content) {
+  var html = `<div class="message">
+        <div class="message__upper-info">
+          <div class="message__upper-info__talkr">
+            ${message.user_name}
+          </div>
+          <div class="message__upper-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__text">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+}else if (message.image) {
+      var html = `<div class="message" data-message-id=${message.id}>
+      <div class="message__upper-info">
+        <div class="message__upper-info__talkr">
+          ${message.user_name}
+        </div>
+        <div class="message__upper-info__date">
+          ${message.created_at}
+        </div>
+      </div>`
+
+        `<div class="message" data-message-id=${message.id}>
                   <div class="message__upper-info">
                     <div class="message__upper-info__talkr">
                       ${message.user_name}
@@ -17,27 +60,11 @@ $(function(){
                   </div>
                   <img src=${message.image} >
                 </div>`
-     return html;
-  }
-    else {
-     var html = `<div class="message">
-                  <div class="message__upper-info">
-                    <div class="message__upper-info__talkr">
-                      ${message.user_name}
-                    </div>
-                    <div class="message__upper-info__date">
-                      ${message.created_at}
-                    </div>
-                  </div>
-                  <div class="message__text">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                  </div>
-                </div>`
-     return html;
-    };
-  }
+      };
+    return html;
+  };
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -64,4 +91,31 @@ $(function(){
         $('.submit-btn').prop('disabled', false);
       })
   })
+
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
